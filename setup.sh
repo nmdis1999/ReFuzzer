362d31188360916b39f2f68b12463249f2bc7169
nmdis1999 <itishree1999@gmail.com>
2025-01-02 18:28:51 +0000

    Updating some things.

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh all
+
+sudo apt update
+sudo apt install -y python3 python3-pip python3-dev build-essential gcc g++ gdb make cmake valgrind clang clang-format clang-tidy
+sudo apt install -y libcurl4-openssl-dev nlohmann-json3-dev pkg-config
+sudo apt install -y llvm
+for version in 15 16 17; do
+    sudo apt install -y clang-$version lldb-$version lld-$version clang-tools-$version
+    sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-$version $((version * 100))
+done
+
+curl -fsSL https://ollama.com/install.sh | sh
+mkdir -p include
+curl -o include/json.hpp https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp
+sleep 5
+
+ollama pull llama2
+ollama pull llama2:latest
+git clone https://github.com/nmdis1999/smartcompilertesting.git
+
